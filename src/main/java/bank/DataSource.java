1857133dc439e3cbfcc13804c12044540d26fe1f
Trpimir Jankoski <111264420+trpimirjankoski@users.noreply.github.com>
2023-12-02 15:00:48 +0000

    Lesson 3

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -3,6 +3,8 @@ package bank;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -20,9 +22,36 @@ public class DataSource {
     
     return connection;
   }
+
+  public static Customer getCustomer(String username){
+      String sql = "select * from customers where username = ?";
+      Customer customer = null;
+      try(Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql)){
+
+        statement.setString(1, username);
+        try(ResultSet resultSet = statement.executeQuery()){
+          customer = new Customer(
+            resultSet.getInt ("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
+        }
+
+      }catch(SQLException e){
+        e.printStackTrace();
+      }
+
+      return customer;
+    }
+
+
     public static void main(String[] args){
-      connect();
+      Customer customer = getCustomer("twest8o@frientfeed.com");
+      System.out.println(customer.getName());
     }
   
 
+    
 }
