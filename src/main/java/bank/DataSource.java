c6bf5f83789a640910b9028af94f4c27ad68ef0c
Trpimir Jankoski <111264420+trpimirjankoski@users.noreply.github.com>
2023-12-03 04:37:41 +0000

    lesson 4

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -46,10 +46,30 @@ public class DataSource {
       return customer;
     }
 
+  public static Account geAccount(int accountId){
+    String sql = "select * from accounts where id =?";
+    Account account = null;
+    try(Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql)){
+        statement.setInt(1, accountId);
 
+        try(ResultSet resultSet = statement.executeQuery()){
+          account = new Account(
+            resultSet.getInt("id"), 
+            resultSet.getString("type"), 
+            resultSet.getDouble("balance"));
+        }
+      }catch(SQLException e){
+        e.printStackTrace();
+      }
+      return account;
+  }
     public static void main(String[] args){
       Customer customer = getCustomer("twest8o@friendfeed.com");
+      Account account = geAccount(customer.getAccountId());
       System.out.println(customer.getName());
+      System.out.println(account.getBalance());
+
     }
   
 
