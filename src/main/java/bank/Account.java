b8c8f9a5a23dd551193802e12ae0d1397573258a
Trpimir Jankoski <111264420+trpimirjankoski@users.noreply.github.com>
2023-12-05 17:16:44 +0000

    update 8

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -1,5 +1,7 @@
 package bank;
 
+import bank.exceptions.AmountException;
+
 public class Account {
 
   private int id;
@@ -38,11 +40,23 @@ public class Account {
     this.balance = balance;
   }
 
-  public void deposit (double amount){
+  public void deposit (double amount) throws AmountException{
+    if (amount<1) {
+      throw new AmountException("The minimum amount for deposit is $1.00");
+    }
+    else {
+      double newBalance = balance + amount;
+      setBalance(newBalance);
+      DataSource.updateAccountBalance(id, newBalance);
+    }
 
   }
 
-  public void withdraw(double amount){
+  public void withdraw(double amount) throws AmountException{
+
+    if (amount<0){
+      throw new AmountException("The withdrawal amount must be greater than 0");
+    }
     
   }
 
--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -64,13 +64,24 @@ public class DataSource {
       }
       return account;
   }
-    public static void main(String[] args){
-      Customer customer = getCustomer("twest8o@friendfeed.com");
-      Account account = geAccount(customer.getAccountId());
-      System.out.println(customer.getName());
-      System.out.println(account.getBalance());
+    
+  public static void updateAccountBalance(int accountId, double balance){
+    String sql = "update accounts set balance = ? where id = ?";
+    try(
+      Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql);
 
+    ){
+      statement.setDouble(1, balance);
+      statement.setInt(2, accountId);
+
+      statement.executeUpdate();
+
+    }catch(SQLException e){
+      e.printStackTrace();
     }
+  }
+  
   
 
     
